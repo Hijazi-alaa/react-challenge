08d962562aa89572438e3b40625bf2c07350be2d
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-04-28 15:28:07 +0000

    Linking Sidebar to its CSS

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import css from "./css/Sidebar.module.css";
+import react from "react";
+
+const Sidebar = () => <div className={css.sidebar}>
+    <a href="#." target="_blank">MyPhotos</a>
+    <a href="#." target="_blank">MyIllustrations</a>
+    <a href="#." target="_blank">My Paintings</a>
+    </div>
+
+export default Sidebar
